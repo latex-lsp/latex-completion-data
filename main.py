9785fffeda26714e125f97c49287f2203ac36ee7
Eric Förster <efoerster@users.noreply.github.com>
2019-07-09 21:51:05 +0200

    Configure logger

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+import logging
+from time import time
+
+
 def main():
-    print("Hello World!")
+    logging.basicConfig(
+        format='%(levelname)-8s %(message)s', level=logging.DEBUG)
+
+    start_time = time()
+    print('Hello World!')
+    end_time = time()
+
+    logging.info('Elapsed time: %d seconds', end_time - start_time)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
