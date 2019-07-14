6e8ac9035ae021d83cd5e49f0b2e802f2405158b
Patrick Förster <pfoerster@users.noreply.github.com>
2019-07-14 13:29:16 +0200

    Merge component database and symbol database

--- main.py
+++ main.py
@@ -6,15 +6,64 @@ import util
 import components
 
 
+class Database:
+    def __init__(self):
+        self.components = []
+
+    def find_package(self, name):
+        for component in self.components:
+            if name:
+                file_name = name + '.sty'
+                if file_name in component.file_names:
+                    return component
+            else:
+                if component.file_names == []:
+                    return component
+
+
+class Component:
+    def __init__(self, file_names, references, commands, environments):
+        self.file_names = file_names
+        self.references = references
+        self.commands = commands
+        self.environments = environments
+        pass
+
+
+class Command:
+    def __init__(self, name):
+        self.name = name
+        self.image = None
+        self.parameters = []
+        pass
+
+
 def main():
     logging.basicConfig(format='%(levelname)-8s %(message)s',
                         level=logging.INFO, filename='latex-completion-data.log', filemode='w')
 
     start_time = timer()
-    symbol_database = symbols.SYMBOL_DATABASE.render()
-    component_database = components.generate_database()
-    end_time = timer()
+    database = Database()
+    database.components.append(Component([], [],
+                                         [Command(x)
+                                          for x in tex.KERNEL_PRIMITIVES.commands],
+                                         tex.KERNEL_PRIMITIVES.environments))
 
+    for component in components.generate_database():
+        commands = [Command(name) for name in component.commands]
+        database.components.append(Component(component.file_names, component.references,
+                                             commands, component.environments))
+
+    for src_package in symbols.SYMBOL_DATABASE.render():
+        dst_package = database.find_package(src_package.name)
+        for src_command in src_package.commands:
+            for dst_command in dst_package.commands:
+                if src_command.name == dst_command.name:
+                    dst_command.image = src_command.image
+                    dst_command.parameters = src_command.parameters
+
+    util.save_json('completion.json', database)
+    end_time = timer()
     logging.info('Elapsed time: %d seconds', end_time - start_time)
 
 
